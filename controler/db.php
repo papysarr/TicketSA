aea646abb4edf581a21ad926f128ac036dc4d891
mbeut <mbeut@example.com>
2019-10-20 13:04:35 +0000

    Deuxieme modif

--- controler/db.php
+++ controler/db.php
@@ -3,7 +3,7 @@
    $dsn = 'mysql:host=localhost;dbname=gesticketsa;port=3306;charset=utf8';
    //b) Selectionner BD
    try {
-        $pdo = new PDO($dsn, 'ouze' , 'malamine10');
+        $pdo = new PDO($dsn, 'papy' , 'King_sa2');
       }
       catch (PDOException $exception) {
          mail('fauxmail@votremail.com', 'PDOException', $exception->getMessage());
---
+++ views/Form_Menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <title>Menu</title>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+          <link rel="stylesheet" type="text/css" href="../public/css/style.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include_once('../includes/menu.php'); ?>
+<?php  include_once('../controler/db.php'); ?>
+
+</ul>
+</body>
+</html>
